35bb5e0cce0bde9d0fab0bdd84f69f130bfca568
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2019-04-07 12:22:43 -0400

    Fix: Wrong user name for db

--- etl.py
+++ etl.py
@@ -92,7 +92,7 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=postgres password=postgres")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
